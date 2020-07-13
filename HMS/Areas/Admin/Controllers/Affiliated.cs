0155e414a18072688605fa3116107b69f042a1d7
Ali Zubair <61495779+AliZ801@users.noreply.github.com>
2020-07-13 19:44:13 +0300

    Add project files.

---
+++ HMS/Areas/Admin/Controllers/Affiliated.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HMS.DataAccess.Data.IRepository;
+using HMS.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HMS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class Affiliated : Controller
+    {
+        private readonly IUnitofWork _unitofWork;
+
+        [BindProperty]
+        public AdminViewModels AVM { get; set; }
+
+        public Affiliated(IUnitofWork unitofWork)
+        {
+            _unitofWork = unitofWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
